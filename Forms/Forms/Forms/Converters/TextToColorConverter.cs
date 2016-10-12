9be024d4909dadf21a02751b85c6a72d4c997540
rukantabula <brukantabula@gmail.com>
2016-10-12 10:14:46 +0200

    using converters to change text value

---
+++ Forms/Forms/Forms/Converters/TextToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Forms.Converters
+{
+    public class TextToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value !=  null)
+            {
+                var text = value.ToString();
+
+                if (text.Contains("B"))
+                {
+                    return Color.Red;
+                }
+            }
+            return Color.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
