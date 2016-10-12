b715d5697caebe10d0a756e7ac30135f7325f50a
rukantabula <brukantabula@gmail.com>
2016-10-12 09:08:01 +0200

    using  INotifyProperty Changed

--- Forms/Forms/Forms/ViewModels/HomeViewModel.cs
+++ Forms/Forms/Forms/ViewModels/HomeViewModel.cs
@@ -1,18 +1,55 @@
-﻿using System;
+﻿using Android.OS;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Forms.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel : INotifyPropertyChanged
     {
-        public Models.TaskModels TaskModel { get; set; }
+        private Models.TaskModels _taskModel;
+        private string _message;
+        public Models.TaskModels TaskModel {
+            get { return _taskModel; }
+            set
+            {
+
+                _taskModel = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Message 
+        {
+
+            get { return _message; }
+    set
+            {
+                _message = value;
+                OnPropertyChanged();
+}
+        }
+        public Command SaveCommand {
+            get
+            {
+                return new Command(() =>
+                {
+                    Message = "Your task : " + _taskModel.Name + " ,"
+                    + _taskModel.Surname + " ," + _taskModel.Email + " was successufully saved!";
+                });
+            }
+            }
+
+        
 
         public HomeViewModel()
         {
-            TaskModel = new Models.TaskModels
+            _taskModel = new Models.TaskModels
             {
                 Name = "Benson",
                 Surname = "Rukantabula",
@@ -20,5 +57,15 @@ namespace Forms.ViewModels
             };
                 }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
+
 }
